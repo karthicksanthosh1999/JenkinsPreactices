67210daef16f0e9da84a5fbcf36bc4ff94dcc99a
karthicksanthosh1999 <101243239+karthicksanthosh1999@users.noreply.github.com>
2022-04-09 10:09:10 +0530

    Update Pubg.java

--- Pubg.java
+++ Pubg.java
@@ -1,6 +1,7 @@
 public class Pubg {
 
 		public static void main(String[] args) {
+				System.out.println("Karthick AWM King");
 				System.out.println("Hello Karthick");
 				System.out.println("Am your friend Jernkins");
 				System.out.println("Am your friend GK");
